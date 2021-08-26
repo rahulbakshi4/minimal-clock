77fb87f77d6d612feff8243779e211237bd8976f
Rahul Bakshi <rahulbakshi3@gmail.com>
2021-08-26 17:01:54 +0530

    added: dark mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import { useState } from 'react';
 import './App.css';
+import Moon from '../src/images/moon.png'
+import Sun from '../src/images/sun.png'
 
 function App() {
 
@@ -21,14 +23,42 @@ function App() {
     let t = new Date().toLocaleTimeString()
     setTime(t)
   }
+  const darktheme = ['bgDark','isdark']
+   const lighttheme = ['bgLight','islight']
+  const [theme,setTheme] = useState(darktheme)
+  const[themeBtnDark,setThemeBtnDark] = useState('none')
+  const[themeBtnLight,setThemeBtnLight] = useState('block')
+
+  function darkBtnHandler(){
+    setThemeBtnDark('none')
+    setTheme(darktheme)
+    setThemeBtnLight('block')
+    
+  }
+  function lightBtnHandler(){
+    setThemeBtnLight("none")
+    setTheme(lighttheme)
+    setThemeBtnDark("block")  
+   }
+  
   setInterval(updateTime, 1000)
   return (
-    <div className="App">
+   
+    <div className={`App ${theme[0]}`} style ={{transition:"1s ease-in"}}>
+     {/* <Navbar/> */}
+     <nav>
+       <button style={{display:themeBtnDark}} onClick={darkBtnHandler}>
+         <img src={Moon} alt="" />
+       </button>
+       <button style={{display:themeBtnLight}} onClick={lightBtnHandler}>
+        <img src={Sun} alt="" />
+       </button>
+     </nav>
       <div className="wrapper">
         <div className="timelist">
-          <div className="time hour-side"><h1>{hour}</h1><p className="details">{phase}</p>
+          <div style={{transition:"1s ease-in"}} className={`time hour-side ${theme[1]}`}><h1>{hour}</h1><p className="details">{phase}</p>
            </div>
-          <div className="time minute-side"><h1>{min}</h1>
+          <div style={{transition:"1s ease-in"}} className={`time minute-side ${theme[1]}`}><h1>{min}</h1>
           <p className="details">{sec}</p>
           </div>
           </div>
