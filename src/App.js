472287379bd6e4ff065b91589d642df9d7f68a40
Rahul Bakshi <rahulbakshi3@gmail.com>
2021-08-26 18:57:27 +0530

    added: footer

--- src/App.js
+++ src/App.js
@@ -14,57 +14,66 @@ function App() {
 
   const time = timeSplit.concat(secSplit)
   let [hour, min, sec, phase] = time
-  
-  if(hour < 10 && hour.length ===1){
-    hour = '0'+hour
+
+  if (hour < 10 && hour.length === 1) {
+    hour = '0' + hour
   }
 
   function updateTime() {
     let t = new Date().toLocaleTimeString()
     setTime(t)
   }
-  const darktheme = ['bgDark','isdark']
-   const lighttheme = ['bgLight','islight']
-  const [theme,setTheme] = useState(darktheme)
-  const[themeBtnDark,setThemeBtnDark] = useState('none')
-  const[themeBtnLight,setThemeBtnLight] = useState('block')
+  const darktheme = ['bgDark', 'isdark']
+  const lighttheme = ['bgLight', 'islight']
+  const [theme, setTheme] = useState(darktheme)
+  const [themeBtnDark, setThemeBtnDark] = useState('none')
+  const [themeBtnLight, setThemeBtnLight] = useState('block')
 
-  function darkBtnHandler(){
+  function darkBtnHandler() {
     setThemeBtnDark('none')
     setTheme(darktheme)
     setThemeBtnLight('block')
-    
+
   }
-  function lightBtnHandler(){
+  function lightBtnHandler() {
     setThemeBtnLight("none")
     setTheme(lighttheme)
-    setThemeBtnDark("block")  
-   }
-  
+    setThemeBtnDark("block")
+  }
+
   setInterval(updateTime, 1000)
   return (
-   
-    <div className={`App ${theme[0]}`} style ={{transition:"1s ease-in"}}>
-     {/* <Navbar/> */}
-     <nav>
-       <button style={{display:themeBtnDark}} onClick={darkBtnHandler}>
-         <img src={Moon} alt="" />
-       </button>
-       <button style={{display:themeBtnLight}} onClick={lightBtnHandler}>
-        <img src={Sun} alt="" />
-       </button>
-     </nav>
+
+    <div className={`App ${theme[0]}`} style={{ transition: "1s ease-in" }}>
+      {/* <Navbar/> */}
+      <nav>
+        <button style={{ display: themeBtnDark }} onClick={darkBtnHandler}>
+          <img src={Moon} alt="" />
+        </button>
+        <button style={{ display: themeBtnLight }} onClick={lightBtnHandler}>
+          <img src={Sun} alt="" />
+        </button>
+      </nav>
       <div className="wrapper">
         <div className="timelist">
-          <div style={{transition:"1s ease-in"}} className={`time hour-side ${theme[1]}`}><h1>{hour}</h1><p className="details">{phase}</p>
-           </div>
-          <div style={{transition:"1s ease-in"}} className={`time minute-side ${theme[1]}`}><h1>{min}</h1>
-          <p className="details">{sec}</p>
+          <div style={{ transition: "1s ease-in" }} className={`time hour-side ${theme[1]}`}><h1>{hour}</h1>
+          <p className="details">{phase}</p>
           </div>
+          <div style={{ transition: "1s ease-in" }} className={`time minute-side ${theme[1]}`}><h1>{min}</h1>
+            <p className={`details`}>{sec}</p>
           </div>
         </div>
-
       </div>
+      <footer>
+    
+    <ul>
+        <li><a href="https://github.com/rahulbakshi4" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a></li>
+        <li><a href="https://twitter.com/rahul_bakshi4" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/rahul-bakshi-552071189/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a></li>
+    </ul>
+    
+</footer>
+    </div>
   );
 }
 
